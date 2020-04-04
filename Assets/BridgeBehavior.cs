bc7580587004c3ebf82855547ddd20a4145d7ef2
harper-wavra <harperwavra@gmail.com>
2020-04-04 11:26:07 -0500

    Add bridge movement on 'e' keyboard press, will changes to when puzzle1 done = true

---
+++ Assets/BridgeBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       // transform.Rotate(0,0,360);
+    }
+    IEnumerator RotateMe(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            StartCoroutine(RotateMe(Vector3.forward * 90, 0.8f));
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            StartCoroutine(RotateMe(Vector3.forward * -90, 0.8f));
+        }
+    }
+}
